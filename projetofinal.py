545aab4ce073b8b8af48663b691c718f08456ee8
Julia Diniz <juliadn@al.insper.edu.br>
2021-11-06 20:37:45 -0300

    Iniciando a parte jogável

--- projetofinal.py
+++ projetofinal.py
@@ -1,4 +1,3 @@
-
 #chamando todas as funções anteriores para serem usadas.
 from adicionanamesa import adiciona_na_mesa
 from cria_pecas import cria_pecas
@@ -31,3 +30,28 @@ iniciando = inicia_jogo(numero_de_jogadores, cria_pecas())
 
 #para saber quais peças aparecerão para o player
 pecas_jogaveis = iniciando['jogadores'][0]
+
+
+#jogando
+
+print('Mesa: ', iniciando['mesa'])
+print(pecas_jogaveis)
+qual_peca = int(input('Escolha a peça: ')) - 1
+analise = posicoes_possiveis(iniciando['mesa'], pecas_jogaveis)
+
+if (qual_peca + 1) in analise:
+    situacao = adiciona_na_mesa(pecas_jogaveis[qual_peca], iniciando['mesa'])
+    del(pecas_jogaveis[qual_peca])
+else:
+    if len(iniciando['monte']) > 0:
+        pecas_jogaveis.append(iniciando['monte'][0])
+    else:
+        print('Você não possui peças e não possui peças no monte, passou a vez.')
+
+
+while True:
+    print('Mesa: ', situacao)
+    print(pecas_jogaveis)
+    qual_peca = int(input('Escolha a peça: ')) -1
+    situacao = adiciona_na_mesa(pecas_jogaveis[qual_peca], situacao)
+    del(pecas_jogaveis[qual_peca])
